f75cb66c1bc0a50e7299ad88754e45b32b59fe3b
DionatanInset <dionatan.vieira@hotmail.com>
2017-09-12 15:31:52 -0300

    Commit Final

--- src/webCrawler.py
+++ src/webCrawler.py
@@ -23,13 +23,7 @@ def get_next_target( page ):
     url = page[start_quote+1:end_quote]
     return url, end_quote
 
-# Compara se existem elementos iguais. Se não insere o elemento em 'p'
-def union( p, q ):
-    for e in q:
-        if e not in p:
-            p.append( e )
-
-# Retorna uma lista com todos os links da página passada como parametro
+# Retorna todos os links da pagina
 def get_all_links( page ):
     links = []
     while True:
@@ -41,61 +35,58 @@ def get_all_links( page ):
             break
     return links
 
-# Procedimento para criar uma strutura de indices
+# Compara se existem elementos iguais. Se não insere o elemento em 'p'
+def union( p, q ):
+    for e in q:
+        if e not in p:
+            p.append( e )
+
+
+#==============================================================================
+
+def lookup( index, keyword ):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
 def add_to_index( index, keyword, url ):
     for entry in index:
         if entry[0] == keyword:
             entry[1].append( url )
             return
-    index.append( [ [keyword], [url] ] )
+    # não encontrado, adicione uma nova entrada
+    index.append( [ keyword, [url] ] )
 
-# Procedimento que retorna a lista de links associado a palavra chave 'word'
-def lookup( index, word ):
-    for entry in index:
-        if entry[0] == word:
-            return entry[1]
-    return []
-
-# Modifica o indice
 def add_page_to_index( index, url, content ):
     words = content.split()
     for word in words:
         add_to_index( index, word, url )
 
-# Retorna uma lista (strutura) contendo o link da página e o conteúdo
-#def crawl_web( seed, page_max ): # argumento opcional 'max_page'
-#    tocrawl = [seed]
-#    crawled = []
-#    index   = []
-#    while tocrawl:
-#        page = tocrawl.pop()
-#        if page not in crawled and len( crawled ) < page_max:
-#            content = get_page( page )
-#            add_page_to_index( index, page, content )
-#            union( tocrawl, get_all_links( content ) )
-#            crawled.append( page )
-#    return index
-def crawl_web( seed, page_max ): # argumento opcional 'max_page'
+#==============================================================================
+
+# Função principal que inicia o rastreamento
+def crawl_web( seed, max_page ): # argumento opcional 'max_page'
     tocrawl = [seed]
     crawled = []
+    index   = []
     while tocrawl:
         page = tocrawl.pop()
-        if page not in crawled and len( crawled ) < page_max:
-            content = get_page( page )
-            union( tocrawl, get_all_links( content ) )
+        if page not in crawled and len( crawled ) < max_page:
+            content_page = get_page( page )
+            add_page_to_index( index, page, content_page )
+            union( tocrawl, get_all_links( content_page ) )
             crawled.append( page )
-    return crawled
-
-# Variavel que armazena os dados coletados
+    return index
+#============================ persistência de dados
 pagesCrawled =[]
 
-# Verifica os parametros de entrada
 url_seed = ''
 max_page = 0
 if len( sys.argv ) == 3:
     url_seed = sys.argv[1]
     max_page = sys.argv[2]
     pagesCrawled = crawl_web( url_seed, max_page )
-    with open("Estrutura Word Links.txt", "w") as links_file: links_file.write( str( pagesCrawled ) )
+    with open("Conteudo Busca.txt", "w") as links_file: links_file.write( str( pagesCrawled ) )
 else:
     print 'Error: Espera-se <programa> <url> <qtd max_page>\n'
